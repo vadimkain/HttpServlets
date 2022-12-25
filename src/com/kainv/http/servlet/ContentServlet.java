7b9d307bf372a51bc4f00cb92aad6d184dcc458a
vadimkain <48841785+vadimkain@users.noreply.github.com>
2022-12-25 02:56:50 +0200

    HTTP. Servlets. 42. Expression Language. EL

--- src/com/kainv/http/servlet/ContentServlet.java
+++ src/com/kainv/http/servlet/ContentServlet.java
@@ -1,5 +1,7 @@
 package com.kainv.http.servlet;
 
+import com.kainv.http.dto.FlightDto;
+import com.kainv.http.service.FlightService;
 import com.kainv.http.util.JspHelper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,6 +10,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
+
+import static java.util.stream.Collectors.*;
 
 /**
  * <h1>HTTP. Servlets. 41. JSP Директивы</h1>
@@ -16,11 +21,40 @@ import java.io.IOException;
  * <p>Так как jsp это тоже сервлет, проксируем на страницу content.jsp. Для этого должны указать полный путь к ней
  * (начиная от директории web):</p>
  * <pre>{@code req.getRequestDispatcher("WEB-INF/jsp/content.jsp").forward(req, resp);}</pre>
+ * <h1>HTTP. Servlets. 42. Expression Language. EL</h1>
+ * <p>В качестве интереса установим в качестве атрибутов в {@code requestScope} нашу информацию о полётах:</p>
+ * <pre>{@code
+ *         List<FlightDto> flightsDto = flightService.findAll();
+ *
+ *         req.setAttribute("flights", flightsDto);
+ * }</pre>
+ * <p>Установим атрибут в {@code sessionScope} ассоциативный массив о полётах:</p>
+ * <pre>{@code
+ *         req
+ *                 .getSession()
+ *                 .setAttribute(
+ *                         "flightMap",
+ *                         flightsDto.stream()
+ *                                 .collect(toMap(FlightDto::getId, FlightDto::getDescription)));
+ * }</pre>
  */
 @WebServlet("/content")
 public class ContentServlet extends HttpServlet {
+    private final FlightService flightService = FlightService.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<FlightDto> flightsDto = flightService.findAll();
+
+        req.setAttribute("flights", flightsDto);
+
+        req
+                .getSession()
+                .setAttribute(
+                        "flightMap",
+                        flightsDto.stream()
+                                .collect(toMap(FlightDto::getId, FlightDto::getDescription)));
+
         req.getRequestDispatcher(JspHelper.getPath("content")).forward(req, resp);
     }
 }
--- web/WEB-INF/jsp/content.jsp
+++ web/WEB-INF/jsp/content.jsp
@@ -11,9 +11,33 @@
     <title>Title</title>
 </head>
 <body>
+<h1>HTTP. Servlets. 42. Expression Language. EL</h1>
+<img src="content-1.png" alt="" width="30%">
+<img src="content-2.png" alt="" width="30%">
+<ul>
+    <li><b>pageScope</b> - можно использовать, когда определяем какие-то данные на уровне страницы.</li>
+    <li><b>requestScope</b> - атрибуты, которые вставили в <code>HttpRequest</code> объект.</li>
+    <li><b>sessionScope</b> - атрибуты, но устанавливаются в объект <code>HttpSession</code>.</li>
+    <li><b>applicationScope</b> - атрибуты, которые устанавливаются в <code>ServletContext</code>.</li>
+    <li><b>initParam</b> - параметры сервлета, которые можем задавать.</li>
+    <li><b>cookie</b> - куки у сессии.</li>
+    <li><b>header</b> - все заголовки, которые передали в request.</li>
+    <li><b>param</b> - параметры запроса, т.е. то что передали после знака вопроса, либо с помощью body, передав header.
+    </li>
+</ul>
+<img src="content-3.png" alt="" width="30%">
 <%@include file="header.jsp" %>
 <div>
-    Content. Русский.
+    <span>Content. Русский.</span>
+    <p><code>requestScope.flights.size()</code> Size: ${requestScope.flights.size()}</p>
+    <p><code>requestScope.flights[1].id</code> Id: ${requestScope.flights[1].id}</p>
+    <p><code>sessionScope.flightMap[1]</code> Id: ${sessionScope.flightMap[1]}</p>
+    <p><code>cookie["JSESSIONID"]</code> JSESSION id: ${cookie["JSESSIONID"]}, unique identifier</p>
+    <p><code>cookie["JSESSIONID"].value</code> JSESSION id: ${cookie["JSESSIONID"].value}, unique identifier</p>
+    <p><code>header["Cookie"]</code> Header: ${header["Cookie"]}</p>
+    <p><code>param.id</code> Param id: ${param.id}</p>
+    <p><code>param.test</code> Param test: ${param.test}</p>
+    <p><code>not empty requestScope.flights</code> Not empty list: ${not empty requestScope.flights}</p>
 </div>
 <%@include file="footer.jsp" %>
 </body>
